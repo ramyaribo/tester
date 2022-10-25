47125341f2ded127107e9129db38ab1aa2d5e428
ramyaribo <114975846+ramyaribo@users.noreply.github.com>
2022-10-25 08:41:16 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,15 @@
+function myadd() {
+        
+
+        
+
+        var x = document.getElementById("id1").value;
+        var y = document.getElementById("id2").value;
+        var z = parseInt(x) + parseInt(y);
+        document.getElementById("result").innerHTML = x+' + '+y+ ' = '+z;
+
+        
+        
+          }
+
+     
